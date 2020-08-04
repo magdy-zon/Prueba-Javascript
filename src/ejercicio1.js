ecb57552fa62210e11a7ee2ad69c1df05a6f06d8
macwho <ganabbisativa@hotmail.com>
2020-08-04 12:39:10 -0500

    commit to send

--- src/ejercicio1.js
+++ src/ejercicio1.js
@@ -1,16 +1,39 @@
 
 
 
-
-
+// No se terminó el ejercicio 1
 
 function getSubArray(elems) {
     masGrande = [];
     for(i=0; i<elems.lenght; i++) {
         n = elems[i];
         for(j=i+1; j<elems.length; j++) {
-            if(elems[j] > n)
-              masGrande[] = n;
+            // if(elems[j] > n)
+              // masGrande[] = n;
         }
     }
 }
+
+
+function obtenAScendente(elems) {
+    asc = [];
+    temp = elems[0];
+    for(i=0; i<elems.length; i++) {
+      n = elems[i];
+      m = elems[i+1];
+      if(n>= temp) {
+        if( n < m) {
+          asc.push(n);
+          asc.push(m);
+          temp = m;
+        } else {
+          asc.push(n);
+          temp = n;
+        }
+      }
+    }
+    console.log(asc);
+}
+
+
+obtenAScendente([2,1,3,4]);
